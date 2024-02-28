54c84843296d8c7571daaaa9724e0837d64edd87
DiegoGoethals <goethalsdiego@gmail.com>
2024-02-28 15:33:14 +0100

    Separate all layers into different projects and add all folders and packages to the correct layers

---
+++ Pri.Api.Pe.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pri.Api.Pe.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
