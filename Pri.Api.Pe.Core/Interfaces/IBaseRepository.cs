cfce7215aeed63554d55549a14dfad088aac3352
DiegoGoethals <goethalsdiego@gmail.com>
2024-03-11 19:46:05 +0100

    Create basic repo interfaces

---
+++ Pri.Api.Pe.Core/Interfaces/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using Pri.Api.Pe.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Api.Pe.Core.Interfaces
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(Guid id);
+        IQueryable<T> GetAll();
+        Task<bool> AddAsync(T toAdd);
+        Task<bool> DeleteAsync(T toDelete);
+        Task<bool> UpdateAsync(T toUpdate);
+        Task<bool> CheckIfExistsAsync(Guid id);
+    }
+}
---
+++ Pri.Api.Pe.Core/Interfaces/IReviewRepository.cs
@@ -0,0 +1,13 @@
+﻿using Pri.Api.Pe.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Api.Pe.Core.Interfaces
+{
+    public interface IReviewRepository : IBaseRepository<Review>
+    {
+    }
+}
