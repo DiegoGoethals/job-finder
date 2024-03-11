116a5b0a834b05bd929490f0942ec6ded08d4b02
DiegoGoethals <goethalsdiego@gmail.com>
2024-03-11 15:39:42 +0100

    Add skill seeding data

--- Pri.Api.Pe.Infrastructure/Data/Seeding/Seeder.cs
+++ Pri.Api.Pe.Infrastructure/Data/Seeding/Seeder.cs
@@ -45,6 +45,24 @@ namespace Pri.Api.Pe.Infrastructure.Data.Seeding
 
             modelBuilder.Entity<IdentityUserRole<Guid>>().HasData(employeeUserRoles.Concat(new[] { employerUserRole })
             );
+
+            var skills = new List<Skill>()
+            {
+                new Skill { Id = Guid.NewGuid(), Name = "Programming" },
+                new Skill { Id = Guid.NewGuid(), Name = "Gardening" },
+                new Skill { Id = Guid.NewGuid(), Name = "Cooking" },
+                new Skill { Id = Guid.NewGuid(), Name = "Cleaning" },
+                new Skill { Id = Guid.NewGuid(), Name = "Driving" },
+                new Skill { Id = Guid.NewGuid(), Name = "Teaching" },
+                new Skill { Id = Guid.NewGuid(), Name = "Writing" },
+                new Skill { Id = Guid.NewGuid(), Name = "Maths" },
+                new Skill { Id = Guid.NewGuid(), Name = "Language" },
+                new Skill { Id = Guid.NewGuid(), Name = "Painting" },
+                new Skill { Id = Guid.NewGuid(), Name = "Photography" },
+                new Skill { Id = Guid.NewGuid(), Name = "Sewing" }
+            };
+
+            modelBuilder.Entity<Skill>().HasData(skills);
         }
 
         private static List<ApplicationUser> GenerateUsers()
